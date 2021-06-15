0f0ccc70f9f6b5b70f51c3299655165f20bc42d4
JOuto <jouni.toni@gmail.com>
2021-06-15 19:34:54 +0300

    https

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/* var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey = fs.readFileSync('certificates/key.pem', 'utf8');
+var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
+
+var credentials = { key: privateKey, cert: certificate }; */
+
+const sslRedirect = require('heroku-ssl-redirect').default;
+
 const express = require('express');
 const cors = require('cors');
 const ContactRouter = require('./Routes/contact.js');
@@ -6,6 +16,8 @@ require('dotenv').config();
 const path = require('path');
 
 const app = express();
+
+app.use(sslRedirect());
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
@@ -16,7 +28,15 @@ app.get('*', (req, res) => {
   console.log(req);
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
+
+//var httpsServer = https.createServer(credentials, app);
+
 const PORT = process.env.PORT || 3001;
+
+/* httpsServer.listen(8443, () => {
+  console.log(`server running on port ${PORT}`);
+}); */
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });

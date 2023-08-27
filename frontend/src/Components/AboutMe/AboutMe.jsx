f012adc5fc2e230eddbab04e8a5bae8ab626add4
JOuto <jouni.toni@gmail.com>
2023-08-27 13:09:22 +0300

    news

--- frontend/src/Components/AboutMe/AboutMe.jsx
+++ frontend/src/Components/AboutMe/AboutMe.jsx
@@ -1,123 +1,78 @@
-import React from 'react';
-import './AboutMe.css';
+import React from "react";
+import "./AboutMe.css";
 
 const AboutMe = () => {
-  //testi
-  return (
-    <div className='aboutMeWrapper'>
-      <p>interview during WOW residency in Amsterdam</p>
-      <h5>
-        {' '}
-        FROM THE HIGH NORTH OF FINLAND TO THE TEMPERATE CLIMATES OF OUR LITTLE
-        COUNTRY, JOUNI TONI’S INTUITIVE AND COLOURFUL PAINTINGS SPEAK TO JUST
-        ABOUT EVERYONE. THERE’S SOMETHING FAMILIAR IN THE BRIGHT, SEEMINGLY
-        RANDOM ABSTRACT SHAPES AND FORMS, EVEN WHEN THEY REFLECT JOUNI’S OWN
-        THOUGHTS AND EXPERIENCES.
-      </h5>
-      <br />
-      <h5>
-        YOU’RE A PAINTER, BUT YOU ALSO MAKE SCULPTURES AND ANIMATIONS. HOW WOULD
-        YOU DESCRIBE YOUR PRACTICE?
-      </h5>
-      <br />
-      I consider myself a painter for the most part, who also uses other
-      techniques. But those techniques all start with the same painter mindset.
-      It’s hard to explain, but I suppose it’s about starting a process with
-      materials and shapes and colours in mind versus something conceptual. The
-      different media allow me to express different aspects of my work;
-      sculptures let me focus more on volume, and animation has those timed
-      images that are quite unlike painting.
-      <br />
-      <h5>
-        YOUR WORK CAN BE PRETTY ABSTRACT, BUT ALSO FEELS FAMILIAR SOMEHOW. HOW
-        WOULD YOU DESCRIBE IT TO SOMEONE WHO’S NEVER SEEN YOUR PAINTINGS?
-      </h5>
-      <br />
-      That’s one of the hardest questions to answer. To me, my paintings are
-      figurative because I know what’s going on there, but I’m never trying to
-      depict the visible reality. I do approach a real phenomenon, or object or
-      landscape, but what I paint is more about my experience of the situation.
-      Naturally, it doesn’t end up being very figurative to the outside world. I
-      usually start with a reference or a concept, and just put out everything
-      that feels connected to that. But sometimes I start with putting shapes
-      and forms out there, which seems random but is unconsciously always
-      connected to the way I feel about things at that moment. Often the result
-      ends up being a reflection on my current state of mind, even though I
-      wasn’t even fully aware of it. It explains something to me, and I think in
-      that sense it could be valuable to others as well.
-      <h5>
-        DO YOU THINK PEOPLE CAN STILL RELATE TO YOUR WORK IF IT’S BASED ON
-        PERSONAL EXPERIENCE?
-      </h5>
-      People are probably not going to feel exactly the same as I did when I
-      painted it. But I believe we all have some kind of common ground. I can’t
-      help but think we’re pretty much identical as human beings, even other
-      life forms are pretty close to humans. If you’re a living, breathing
-      creature on this planet, you will have similar basic reactions and
-      emotions. Maybe these abstract shapes are one way of communicating our
-      common ground. I think most people can find something that resonates with
-      them. We all live much more similar lives than we sometimes think.
-      <h5>
-        YOU CAME TO AMSTERDAM TO DO A RESIDENCY AT THE RIJKSAKADEMIE. HOW DID
-        YOUR TIME THERE INFLUENCE YOUR WORK?
-      </h5>
-      It helped in every possible way. I came to the Rijksakademie because I
-      felt a bit stuck in my work in Finland. Being around other talented and
-      professional people, seeing how they work and run their practice, it
-      helped me expand my techniques and made me see my work in a different
-      light. Before, I was very into doing my own thing with a certain tunnel
-      vision. The Rijksakademie really makes you question what it is you’re
-      doing. I went back to Finland for a while when I finished, but I decided
-      that, at this point in my career, it would be better to stay in the
-      Netherlands for a while.
-      <h5>WHY IS THAT?</h5>
-      At the time, I felt like there weren’t many possibilities for me to
-      progress in Finland. Moving back to Finland would feel like taking a step
-      backwards, and I wanted new challenges. One important thing in art and
-      life in general, for me anyway, is that I have to put myself into some
-      kind of difficult or uncomfortable situation in order to progress. Finland
-      doesn’t provide that situation for me. I need an environment that pushes
-      me forward.
-      <h5>WHAT HAS IT BEEN LIKE TO LIVE HERE AT WOW?</h5>
-      It was actually a bit challenging at first, to work in the same place
-      where you live. I wasn’t sure I’d be able to get any work done here. It’s
-      hard to focus when you’re working at ‘home’, and I didn’t like the idea of
-      paint fumes coming into my bedroom. I moved here last February, and the
-      first few months I didn’t do any paintings at all. But eventually, I got
-      used to it. I had a few ideas, so I just started painting. It’s not the
-      most ideal working situation, but nothing ever really is.
-      <h5>WHERE CAN WE SEE THOSE NEW PAINTINGS?</h5>I started working on them
-      last autumn, I set a deadline for myself to start making work even though
-      there was no exhibition planned. Actually, right when I finished the work
-      I met the curators here at WOW who asked me if I wanted to exhibit my
-      painting in the Vertical Gallery. So everything came together. I still
-      want to finish a few things, maybe add some small sculptures. There is no
-      set date yet, but it should be happening sometime in mid-February. (Edit:
-      find the exhibition’s page here).
-      <h5>
-        WHERE DO YOU FIND THE MOTIVATION TO KEEP MAKING ART? SETTING DEADLINES
-        FOR YOURSELF TAKES A LOT OF DISCIPLINE.
-      </h5>
-      <br />
-      It can be difficult. Especially when you bring logic into the equation,
-      you start asking yourself things like: why am I doing this, what’s the
-      point? But the urge to make something always comes back. It’s this need to
-      express, and as an artist there’s not necessarily any other way to do it.
-      I’m also pretty competitive, which sounds odd in a world where good and
-      bad are subjective. It helps keep me going. Obviously, the ideal situation
-      would be making art just for the sake of art. The paintings I made for the
-      exhibition were like that, and I end up caring less about the competitive
-      side. It doesn’t matter if it’s just me who likes it. But you can’t always
-      be in that higher mindset, external pressure is good sometimes. Of course,
-      making art is incredibly uncertain. Because you don’t want to do something
-      you already know. You have to always go into the unknown if you want to
-      make something new. If you want certainty, art is the worst place to be.
-      Although nowadays, there’s very little real certainty anymore, even if
-      you’re not an artist.
-      <br />
-      <br />
-      <p>Words by Suzanna Knight </p>
-    </div>
-  );
-};
+//testi
+    return (
+        <div className="aboutMeWrapper">
+            <p>interview during WOW residency in Amsterdam</p>
+            <h5> FROM THE HIGH NORTH OF FINLAND TO THE TEMPERATE CLIMATES OF OUR LITTLE COUNTRY, JOUNI TONI’S INTUITIVE AND COLOURFUL PAINTINGS SPEAK TO JUST ABOUT EVERYONE. THERE’S SOMETHING FAMILIAR IN THE BRIGHT, SEEMINGLY RANDOM ABSTRACT SHAPES AND FORMS, EVEN WHEN THEY REFLECT JOUNI’S OWN THOUGHTS AND EXPERIENCES.</h5>
+<br/>
+<h5>YOU’RE A PAINTER, BUT YOU ALSO MAKE SCULPTURES AND ANIMATIONS. HOW WOULD YOU DESCRIBE YOUR PRACTICE?</h5>
+<br/>
+I consider myself a painter for the most part, who also uses other techniques. But those techniques all start with the same painter mindset. 
+It’s hard to explain, but I suppose it’s about starting a process with materials and shapes and colours in mind versus something conceptual.
+
+ The different media allow me to express different aspects of my work; sculptures let me focus more on volume, and animation has those timed images that are quite unlike painting.
+<br/>
+<h5>YOUR WORK CAN BE PRETTY ABSTRACT, BUT ALSO FEELS FAMILIAR SOMEHOW. HOW WOULD YOU DESCRIBE IT TO SOMEONE WHO’S NEVER SEEN YOUR PAINTINGS?</h5><br/>
+That’s one of the hardest questions to answer. To me, my paintings are figurative because I know what’s going on there, but I’m never trying
+ to depict the visible reality. I do approach a real phenomenon, or object or landscape, but what I paint is more about my experience of the
+  situation. Naturally, it doesn’t end up being very figurative to the outside world. I usually start with a reference or a concept, and just 
+  put out everything that feels connected to that. But sometimes I start with putting shapes and forms out there, which seems random but is 
+  unconsciously always connected to the way I feel about things at that moment. Often the result ends up being a reflection on my current 
+  state of mind, even though I wasn’t even fully aware of it. It explains something to me, and I think in that sense it could be valuable to others as well.
+
+  <h5>DO YOU THINK PEOPLE CAN STILL RELATE TO YOUR WORK IF IT’S BASED ON PERSONAL EXPERIENCE?</h5>
+People are probably not going to feel exactly the same as I did when I painted it. But I believe we all have some kind of common ground. 
+I can’t help but think we’re pretty much identical as human beings, even other life forms are pretty close to humans. If you’re a living,
+ breathing creature on this planet, you will have similar basic reactions and emotions.
+
+Maybe these abstract shapes are one way of communicating our common ground. I think most people can find something that resonates with 
+them. We all live much more similar lives than we sometimes think.
+
+<h5>YOU CAME TO AMSTERDAM TO DO A RESIDENCY AT THE RIJKSAKADEMIE. HOW DID YOUR TIME THERE INFLUENCE YOUR WORK?</h5>
+It helped in every possible way. I came to the Rijksakademie because I felt a bit stuck in my work in Finland. Being around other 
+talented and professional people, seeing how they work and run their practice, it helped me expand my techniques and made me see
+ my work in a different light. Before, I was very into doing my own thing with a certain tunnel vision. The Rijksakademie really
+  makes you question what it is you’re doing. I went back to Finland for a while when I finished, but I decided that, at this point 
+  in my career, it would be better to stay in the Netherlands for a while.
+
+  <h5>WHY IS THAT?</h5>
+At the time, I felt like there weren’t many possibilities for me to progress in Finland. Moving back to Finland would feel
+like taking a step backwards, and I wanted new challenges. One important thing in art and life in general, for me anyway, 
+is that I have to put myself into some kind of difficult or uncomfortable situation in order to progress. Finland doesn’t 
+provide that situation for me. I need an environment that pushes me forward.
+
+<h5>WHAT HAS IT BEEN LIKE TO LIVE HERE AT WOW?</h5>
+It was actually a bit challenging at first, to work in the same place where you live. I wasn’t sure I’d be able to get 
+any work done here. It’s hard to focus when you’re working at ‘home’, and I didn’t like the idea of paint fumes coming 
+into my bedroom. I moved here last February, and the first few months I didn’t do any paintings at all. But eventually, 
+I got used to it. I had a few ideas, so I just started painting. It’s not the most ideal working situation, but nothing ever really is.
+
+<h5>WHERE CAN WE SEE THOSE NEW PAINTINGS?</h5>
+I started working on them last autumn, I set a deadline for myself to start making work even though there was no exhibition 
+planned. Actually, right when I finished the work I met the curators here at WOW who asked me if I wanted to exhibit my painting 
+in the Vertical Gallery. So everything came together. I still want to finish a few things, maybe add some small sculptures.
+ There is no set date yet, but it should be happening sometime in mid-February. (Edit: find the exhibition’s page here).
+
+ <h5>WHERE DO YOU FIND THE MOTIVATION TO KEEP MAKING ART? SETTING DEADLINES FOR YOURSELF TAKES A LOT OF DISCIPLINE.</h5><br/>
+It can be difficult. Especially when you bring logic into the equation, you start asking yourself things like: why 
+am I doing this, what’s the point? But the urge to make something always comes back. It’s this need to express, and 
+as an artist there’s not necessarily any other way to do it. I’m also pretty competitive, which sounds odd in a world 
+where good and bad are subjective. It helps keep me going. Obviously, the ideal situation would be making art just for
+ the sake of art. The paintings I made for the exhibition were like that, and I end up caring less about the competitive 
+ side. It doesn’t matter if it’s just me who likes it. But you can’t always be in that higher mindset, external pressure 
+ is good sometimes.
+
+Of course, making art is incredibly uncertain. Because you don’t want to do something you already know. You have to 
+always go into the unknown if you want to make something new. If you want certainty, art is the worst place to be. 
+Although nowadays, there’s very little real certainty anymore, even if you’re not an artist.<br/><br/>
+
+ 
+
+<p>Words by Suzanna Knight </p>
+        </div>
+    )
+}
 export default AboutMe;
--- frontend/src/Components/News/News.jsx
+++ frontend/src/Components/News/News.jsx
@@ -5,17 +5,12 @@ import './News.css';
 const News = () => {
   return (
     <div className='newsWrapper'>
-<h4>Current:</h4>
-<br />
-      <p>
-       1.4.-24.4. Solo exhibition in Galleria Ama, Helsinki
-      </p>
+      {/* <h4>Current:</h4> */}
+      <br />
+      <p></p>
       <h4>Upcoming:</h4>
       <br />
-      <p>
-        13.4.- 18.4. KunstRai, Amsterdam
-      </p>
-      
+      <p>2024 Galleria Ama, Helsinki</p>
     </div>
   );
 };

526f66095261f681848f98ecd2bf2b78570cc261
JOuto <jouni.toni@gmail.com>
2023-08-28 16:23:23 +0300

    testi

--- frontend/src/Components/News/News.jsx
+++ frontend/src/Components/News/News.jsx
@@ -10,7 +10,7 @@ const News = () => {
       <p></p>
       <h4>Upcoming:</h4>
       <br />
-      <p>2024 Solo exhibition in Galleria Ama, Helsinki</p>
+      <p>2024 Galleria Ama, Helsinki</p>
     </div>
   );
 };

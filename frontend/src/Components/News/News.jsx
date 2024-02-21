211177c09e8e4526169d0e63ac0c20844b30f2e2
JOuto <jouni.toni@gmail.com>
2024-02-21 15:44:08 +0200

    news

--- frontend/src/Components/News/News.jsx
+++ frontend/src/Components/News/News.jsx
@@ -6,14 +6,15 @@ const News = () => {
   return (
     <div className='newsWrapper'>
       {/* <h4>Current:</h4> */}
-  
+
       <p></p>
       <h4>Upcoming:</h4>
       <br />
       <p>2024 solo exhibition in Galleria Ama, Helsinki</p>
       <br />
       <p>2024 Enter Art Fair, Copenhagen (Rademakers Gallery)</p>
-     
+      <br />
+      <p>2024 Art Antwerpen (Rademakers Gallery)</p>
     </div>
   );
 };

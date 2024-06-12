4999076625fd93104018acf5339791a92c1c90da
JOuto <jouni.toni@gmail.com>
2024-06-12 22:34:38 +0300

    news

--- frontend/src/Components/News/News.jsx
+++ frontend/src/Components/News/News.jsx
@@ -15,10 +15,15 @@ const News = () => {
         Galerie SANAA, Utrecht
       </p>
       <br />
+      <p>2024 Art Center Purnu 15.6.- 11.8., Orivesi, Finland</p>
+      <br />
       <br />
       <h4>Upcoming:</h4>
       <br />
-      <p>2024 Art Center Purnu 15.6.- 11.8., Orivesi, Finland</p>
+      <p>
+        2024 "Shelter From The Storm", MAKASIINI Contemporary, Turku, Finland
+      </p>
+
       <br />
       <p>2024 Enter Art Fair, Copenhagen (Rademakers Gallery)</p>
       <br />
